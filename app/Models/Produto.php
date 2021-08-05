f332614f40f5d621d843c5535c7ca2234d906f28
Ivini <johnatan.ivini@gmal.com>
2021-08-05 20:48:17 -0300

    Adicionar fillables e relação categoria

--- app/Models/Produto.php
+++ app/Models/Produto.php
@@ -4,13 +4,44 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Produto extends Model
 {
     use HasFactory;
-        
+
     /**
      * @var string $table
      */
-    protected $table = 'tb_produto';
+    protected string $table = 'tb_produto';
+
+    /**
+     * 
+     * @var array $dates
+     */
+    protected array $dates = [
+        'data_cadastro'
+    ];
+
+    /**
+     * Campos que podem ser salvos
+     *
+     * @var array $fillable
+     */
+    protected array $fillable = [
+        'data_cadastro',
+        'nome_produto',
+        'valor_produto',
+        "id_categoria_produto"
+    ];
+
+    /**
+     * Categoria Produto
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function categoriaProduto(): BelongsTo
+    {
+        return $this->belongsTo(CategoriaProduto::class, 'id_categoria_produto', 'id_categoria_planejamento');
+    }
 }
