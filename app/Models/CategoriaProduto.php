b2753a1f91bd2d22439a1a91a775df28cd59c175
Ivini <johnatan.ivini@gmal.com>
2021-08-05 20:51:49 -0300

    Adicionar fillables e relacao de produtos

--- app/Models/CategoriaProduto.php
+++ app/Models/CategoriaProduto.php
@@ -4,10 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class CategoriaProduto extends Model
 {
     use HasFactory;
 
-    protected $table = 'tb_categoria_produto';
+    protected string $table = 'tb_categoria_produto';
+
+    protected array $fillable = [
+        'nome_categoria'
+    ];
+
+    /**
+     * Retorna para categoria especifica
+     * Todos os produtos a ela relacionados
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function produtos(): HasMany
+    {
+        return $this->hasMany(Produto::class, 'id_categoria_planejamento', 'id_categoria_produto');
+    }
 }
