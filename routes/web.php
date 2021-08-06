649391108b77279beeb38289237447ef7d6188b5
Ivini <johnatan.ivini@gmal.com>
2021-08-06 02:47:25 -0300

    Adicionar rota de lista de produtos

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProdutoController;
+use App\Models\Produto;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    echo "";
 });
+
+Route::get('/produtos', 'ProdutoController@index');
