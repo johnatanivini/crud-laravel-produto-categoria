b62e6461fd21b0e0d4f3d7e7e96c0e38442ca3fc
Ivini <johnatan.ivini@gmal.com>
2021-08-06 02:00:31 -0300

    Ajustar comentario

--- app/Repositories/Eloquent/BaseRepository.php
+++ app/Repositories/Eloquent/BaseRepository.php
@@ -9,18 +9,27 @@ use Illuminate\Database\Eloquent\Model;
 /**
  *  Base dos repositorios
  */
-class BaseRepository implements EloquentRepositoryInterface {
-
+class BaseRepository implements EloquentRepositoryInterface
+{
 
     public function __construct(protected Model $model)
     {
     }
 
+    /**
+     *
+     * @param integer $id
+     * @return \Illuminate\Database\Eloquent\Model|null
+     */
     public function find(int $id): ?Model
     {
         return $this->model->find($id);
     }
-
+    /**
+     *
+     * @param array $attributos
+     * @return \Illuminate\Database\Eloquent\Model
+     */
     public function create(array $attributos): Model
     {
         return $this->model->create($attributos);
