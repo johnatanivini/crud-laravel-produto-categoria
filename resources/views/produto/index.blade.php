caed9cfc55ffebca3b77ef91126a5ae5eca7e787
Ivini <johnatan.ivini@gmal.com>
2021-08-06 10:26:17 -0300

    Ajustar lista de produtos

--- resources/views/produto/index.blade.php
+++ resources/views/produto/index.blade.php
@@ -10,24 +10,26 @@
 </div>
 <div class="d-flex flex-column justify-content-center">
     @foreach ($produtos as $produto)
-        <div class="d-flex flex-row justify-content-center">
-            <div class="p2">
+    <hr class="line m-0 p-0" />
+        <div class="d-flex flex-row justify-content-center bg-light">
+            <div class="p2 mr2">
                 {{$produto->id_produto}}
             </div>
-            <div class="p2">
+            <div class="p2 mr-2">
                 {{$produto->nome_produto}}
             </div>
-            <div class="p2">
+            <div class="p2 mr-2">
                 {{$produto->valor_roduto}}
             </div>
-            <div class="p2">
+            <div class="p2 mr-2">
                 {{$produto->data_cadastro->format('d-m-Y')}}
             </div>
-            <div class="p2">
+            <div class="p2 m2">
                <a href="{{ route('produto.editar', ['id'=>$produto->id_produto]) }}" class="btn btn-info">Editar</a>
-               <a href="{{ route('produto.deletar', ['id'=>$produto->id_produto]) }}" class="btn btn-danger">Excluir</a>
+               <a href="{{ route('produto.delete', ['id'=>$produto->id_produto]) }}" class="btn btn-danger">Excluir</a>
             </div>
         </div>
+        
     @endforeach
 </div>
 @endsection
